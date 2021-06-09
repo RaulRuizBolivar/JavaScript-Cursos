22f72a23e42deabe2a0f8e71e1559d2d666fc600
Raúl Ruiz Bolivar <raul.ruiz.bolivar@gmail.com>
2021-06-09 13:25:20 +0200

    Hoisting

--- funciones.js
+++ funciones.js
@@ -25,3 +25,7 @@ function sumar(a,b){
   var resultado = a+b;
   return resultado;
 }
+
+
+//Las funciones declarativas si so hace Hoisting y en las expresivar o anónimas no
+//El Hoisting lo que quiere decir es que se puede hacer llamar a la función antes de declararla.
