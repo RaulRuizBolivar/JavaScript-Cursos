8a20782c57fdedc91f9a004f2cef42e2f5c21651
Raúl Ruiz Bolivar <raul.ruiz.bolivar@gmail.com>
2021-06-14 15:40:14 +0200

    Clase de operadores

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # JavaScript-Cursos
-Los archivos que utilizo en mis cursos de JavaScript
+* Aquí mi apuntes de JavaScript
+** Booleanos
+Con la función Boolean() se puede comprobar el valor booleano del parámetro.
+Por defecto, los valores false son:
+    0 , null , NaN , undefined , false , ""
+Por defecto los valores true son :
+    Cualquier número distinto de 0 , Cualquier string con al menos un valor , [] , {} , true
+** Coerción
+La coerción es el cambio de tipo de un valor, hay dos tipos:
+    *** Coerción pasiva
+    Es cuando no se impone directamente el tipo de valor al que se tiene que cambiar
+
---
+++ operaciones.js
@@ -0,0 +1,31 @@
+/*
+ * Operadores binarios:
+ * + Suma dos valores, en caso de ser dos tipos distintos siendo uno un string, los concatena
+ * - Resta
+ * * Multiplica
+ * / Divide
+ *
+ *
+ * Operador de asignación
+ * = Asigna un valor
+ *
+ *
+ * Operadores de comparación:
+ * == Compara el contenido de dos valores, si el tipo es distinto no importa
+ * === Compara el contenido de dos valores y el tipo, de forma estricta.
+ * < Compara dos numeros, si el primero es menor que el segundo es true
+ * > Compara dos números, si el primero es mayor que el segundo es true
+ * <= Compara dos valores, si el primero es menor o igual al segundo, es true
+ * >= Compara dos valores, si el primero es mayor o igual al segundo, es true
+ *
+ *
+ * Operadores lógicos:
+ * ! Negación, niega lo que le sigue (!true -> false)
+ * && Signo de puerta lógica AND
+ * || Signo de puerta lógica OR
+ *
+ * Operadores incrementales:
+ * ++ Incrementa el valor en uno, si es antes del valor, se añade antes de la asignación, si no, después
+ * += Incrementa con el valor que se indica después el valor de lo que le precede
+ *
+ */
