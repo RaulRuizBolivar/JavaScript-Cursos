fe984f3c633624b1c7222d1abdd4ef964dd51cf9
Raúl Ruiz Bolivar <raul.ruiz.bolivar@gmail.com>
2021-06-14 13:49:33 +0200

    Booleans

---
+++ boolean.js
@@ -0,0 +1,5 @@
+/*
+ * Con la función Boolean() se puede comprobar el valor booleano del valor,
+ * Son falsos=[0,null,NaN,undefined,false,""];
+ * Son verdaderos=[1,"s",[],{},function(){},true];
+ */
