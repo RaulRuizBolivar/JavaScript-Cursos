3c53200de5c551d9947dcc983e77754c76b7313e
Raúl <raul.ruiz.bolivar@gmail.com>
2021-06-16 13:50:49 +0200

    switch

---
+++ switch.js
@@ -0,0 +1,11 @@
+var numero = 2;
+switch ( numero ) {
+  case 1 : 
+    console.log("Soy uno");
+    break;
+  case 10:
+    console.log("soy diez");
+    break;
+  default:
+    console.log("Nos soy nada");
+}
